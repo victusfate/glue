d2d63800e16512ea986063098ff0a41bf24f80a4
Mark Essel <messel@gmail.com>
2013-01-19 16:43:48 -0500

    finished getting all the variations to work for variable bit rate stream containers with local and remote references

--- glue.js
+++ glue.js
@@ -3,13 +3,14 @@ var g               = require('./lib/glueUtil');
 var arguments       = process.argv.splice(2);
 var inName          = arguments[0];
 
-g.getStreamFile(inName, function(err, name) {
+g.getStreamFile(inName, function(err, name, oname) {
+    console.log('getStreamFile callback called');
     if (err) {
         console.log(err); 
         throw err; 
     }
     else {
-        g.launchGlue(name);
+        g.launchGlue(name,oname);
     }
 }.bind(this));
 
