2c6f67aeb7790b7f1fc2bec2e6d9f4f7d20769ea
Mark Essel <messel@gmail.com>
2013-01-27 06:06:25 -0500

    added glue exec to bin

--- glue.js
+++ glue.js
@@ -3,6 +3,11 @@ var g               = require('./lib/glueUtil');
 var arguments       = process.argv.splice(2);
 var inName          = arguments[0];
 
+if (arguments < 2) {
+	console.log('usage: glue stream.m3u8 or glue http://domain/stream.m3u8');
+	process.exit(1);
+}
+
 g.getStreamFile(inName, function(err, name, oname) {
     console.log('getStreamFile callback called');
     if (err) {
