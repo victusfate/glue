b3c7e4311ed44955da1f08885389c23e00296fd4
Mark Essel <messel@gmail.com>
2016-02-23 18:06:01 -0500

    clean repo

---
+++ README.md
@@ -0,0 +1,15 @@
+![glue](https://github.com/victusfate/glue/raw/master/glue.jpg)
+javascript ffmpeg glue tool
+===
+
+simple javascript tool to glue together ts files from streams
+
+to install:
+<pre>npm install stream-glue</pre>
+
+or grab the repo
+
+to use:
+<pre>stream-glue http://streamUrl.m3u8</pre>
+
+
---
+++ glue.js
@@ -0,0 +1,25 @@
+var g               = require('./lib/glueUtil');
+
+var arguments       = process.argv.splice(2);
+var inName          = arguments[0];
+
+if (arguments < 2) {
+	console.log('usage: glue stream.m3u8 or glue http://domain/stream.m3u8');
+	process.exit(1);
+}
+
+g.getStreamFile(inName, function(err, name, audioName, oname, urlPath) {
+    console.log('getStreamFile callback called',name);
+    if (err) {
+        console.log(err); 
+        throw err; 
+    }
+    else {
+        g.launchGlue(urlPath,name,audioName,oname);
+    }
+}.bind(this));
+
+process.on('uncaughtException', function(err) {
+    console.log(err);
+});
+
